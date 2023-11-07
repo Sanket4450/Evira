eb6fc1785d39a5e4bcc06bba91f383b4355d08ed
Sanket4450 <www.talaviyasanket50@gmail.com>
2023-11-07 12:54:08 +0530

    swagger ui loading issue fixed

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const yaml = require('yaml')
 const swaggerUi = require('swagger-ui-express')
+const { SwaggerUIBundle, SwaggerUIStandalonePreset } = require('swagger-ui-dist')
 
 const yamlFile = fs.readFileSync(path.join(process.cwd(), 'api_docs/Evira.yaml'), 'utf8')
 const swaggerDocument = yaml.parse(yamlFile)
