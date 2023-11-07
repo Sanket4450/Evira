81f0ed9b940e418a4a9118f79703a06df1051c20
Sanket4450 <www.talaviyasanket50@gmail.com>
2023-11-07 11:41:18 +0530

    some path bugs fixed

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,20 +1,12 @@
 const winston = require('winston')
-const path = require('path')
 
 const { createLogger, format, transports } = winston
 const { combine, timestamp, json } = format
-
-// const fileOptions = {
-//     filename: path.resolve(process.cwd(), 'logs/app.log'),
-//     maxsize: 2097152,
-//     maxFiles: 5
-// }
 
 const logger = createLogger({
     level: 'info',
     format: combine(timestamp(), json()),
     transports: [
-        // new transports.File(fileOptions), // to store all logs in a log file
         new transports.Console()
     ]
 })
