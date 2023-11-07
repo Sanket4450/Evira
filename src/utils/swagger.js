16ae74bf3fcd18db12c86ad06cdcaf4e62d85eb2
Sanket4450 <www.talaviyasanket50@gmail.com>
2023-11-07 11:47:02 +0530

    swagger docs issue fixed

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -4,7 +4,7 @@ const path = require('path')
 const yaml = require('yaml')
 const swaggerUi = require('swagger-ui-express')
 
-const yamlFile = fs.readFileSync(path.join(__dirname, '../../api_docs/Evira.yaml'), 'utf8')
+const yamlFile = fs.readFileSync(path.join(process.cwd(), 'api_docs/Evira.yaml'), 'utf8')
 const swaggerDocument = yaml.parse(yamlFile)
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
